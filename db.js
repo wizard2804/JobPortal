9c78dee553a6adba5cb5da039b60c39988f7eb15
wizard2804 <duseatharva@gmail.com>
2024-11-11 22:13:08 +0530

    Initial commit for JobPortal Project

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ dbConnect()
 async function dbConnect(){
 
      try {
-         await mongoose.connect('mongodb+srv://saket:kaleenbhaiya@cluster0.7aygt.mongodb.net/jobportal' , {useNewUrlParser : true});
+         await mongoose.connect('mongodb+srv://wizard123:atharva123@cluster0.vb52r.mongodb.net/' , {useNewUrlParser : true});
          console.log('Mongo DB Connection successful')
      } catch (error) {
          console.log('Mongo DB Connection failed')
